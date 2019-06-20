6e2e0252158f580b5461e7556fef9f57fb910e0d
Xiaohan Gong <7930083@qq.com>
2019-06-20 15:35:38 +0800

    Remove Duplicate

--- LeetcodeArray/src/RemoveDuplicate.java
+++ LeetcodeArray/src/RemoveDuplicate.java
@@ -1,20 +1,38 @@
+import java.util.Arrays;
+
 /**
  * Created by 79300 on 2019/6/16.
  */
 public class RemoveDuplicate {
     public int removeDuplicates(int[] nums) {
-        int i = 0,j=0;
+        int i = 0, j = 0;
+        //双指针法，i代表下一个填入的位置，j代表遍历到的位置，当j遍历到数组末尾时结束
         while (j < nums.length) {
+            //把当前的值存入i的位置
             int current_value = nums[j];
             nums[i] = current_value;
             i++;
+            //把j不断后移，跳过相同的值
             while (j < nums.length && nums[j] == current_value) j++;
         }
         return i;
+/*        if(nums.length==0) return 0 ;
+        int i = 0,j=0;
+        int current_value = nums[j];
+        while (j < nums.length) {
+            if(nums[j]==current_value){
+                j++;
+                continue;
+            }
+            current_value = nums[j];
+            i++;
+            nums[i] = current_value;
+        }
+        return i+1;*/
     }
 
     public static void main(String[] args) {
         RemoveDuplicate rd = new RemoveDuplicate();
-        System.out.println(rd.removeDuplicates(new int[]{1, 1, 2}));
+        System.out.println(rd.removeDuplicates(new int[]{}));
     }
 }
--- LeetcodeArray/src/RemoveDuplicateII.java
+++ LeetcodeArray/src/RemoveDuplicateII.java
@@ -1,20 +1,53 @@
+import java.util.Arrays;
+
 /**
  * Created by 79300 on 2019/6/17.
  */
 public class RemoveDuplicateII {
-    public int removeDuplicates(int[] nums) {
-        int i = 0,j=0;
+    public int removeDuplicatesII(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0, j = 0;
+        //双指针法，i代表下一个填入的位置，j代表遍历到的位置，当j遍历到数组末尾时结束
         while (j < nums.length) {
-            int current_value = nums[j];
+            //先把当前的值存到i位置，并设置appear_count为一次
+            int current_value = nums[j], appear_count = 1;
             nums[i] = current_value;
             i++;
-            while (j < nums.length && nums[j] == current_value) j++;
+            //当j与当前值相等时，j往后移。即跳过重复的元素
+            while (j < nums.length && nums[j] == current_value) {
+                j++;
+                //如果元素是第一次出现重复，填入i的位置并修改i的值。多次重复不再填入i的位置。
+                if (i < nums.length && j < nums.length && nums[j] == current_value && appear_count < 2) {
+                    nums[i] = current_value;
+                    appear_count++;
+                    i++;
+                }
+            }
         }
-        return i;
+        return i - 1;
+/*
+        if (nums.length == 0) return 0;
+        int i = 0, j = 0;
+        int current_value = nums[j],appear_count=0;
+        while (j < nums.length) {
+            if(nums[j] ==current_value){
+                j++;
+                appear_count++;
+                if(appear_count<=2){
+                    nums[i]=current_value;
+                    i++;
+                }
+                continue;
+            }
+            current_value = nums[j];
+            appear_count =0;
+        }
+        System.out.println(Arrays.toString(nums));
+        return i;*/
     }
 
     public static void main(String[] args) {
-        RemoveDuplicate rd = new RemoveDuplicate();
-        System.out.println(rd.removeDuplicates(new int[]{1, 1, 2}));
+        RemoveDuplicateII rd = new RemoveDuplicateII();
+        System.out.println(rd.removeDuplicatesII(new int[]{0, 0, 1, 1, 2, 3, 3}));
     }
 }
