92636d645ae7907e55afe5e7c8cba5a2ab0643e8
Clover <xiaohangong04@gmail.com>
2019-09-22 15:32:02 -0700

    tree

--- LeetcodeTree/src/InvertBinaryTree.java
+++ LeetcodeTree/src/InvertBinaryTree.java
@@ -1,13 +1,37 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by 79300 on 2019/9/20.
  */
 public class InvertBinaryTree {
+/*  recursive
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
         TreeNode right = invertTree(root.left);
         TreeNode left = invertTree(root.right);
         root.left = right;
         root.right = left;
+        return root;
+    }*/
+
+    //iterative
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            TreeNode current = queue.poll();
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+            if(current.left!=null){
+                queue.offer(current.left);
+            }
+            if(current.right!=null){
+                queue.offer(current.right);
+            }
+        }
         return root;
     }
 }
