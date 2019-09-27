f014d82193352d19d0e598db9576e674129e246f
Clover <xiaohangong04@gmail.com>
2019-09-26 23:49:47 -0700

    tree

---
+++ LeetcodeTree/src/BinaryTreeRightSideView.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by 79300 on 2019/9/26.
+ */
+public class BinaryTreeRightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int value=0;
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                value = node.val;
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+                size--;
+            }
+            result.add(value);
+        }
+        return result;
+    }
+}
