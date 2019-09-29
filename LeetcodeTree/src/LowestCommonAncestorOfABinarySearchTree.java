12badc2e633f53d1abb7e05eb99dda1983447654
Clover <xiaohangong04@gmail.com>
2019-09-28 17:46:15 -0700

    tree

--- LeetcodeTree/src/LowestCommonAncestorOfABinarySearchTree.java
+++ LeetcodeTree/src/LowestCommonAncestorOfABinarySearchTree.java
@@ -3,7 +3,11 @@
  */
 public class LowestCommonAncestorOfABinarySearchTree {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
+        if (root == null) return null;
+        if (root == p || root == q) return root;
+        if (p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q);
+        if (p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q);
+        return root;
     }
 
 }
