92468c51eb33a4f37eb353cf269cc23623675a11
Clover <xiaohangong04@gmail.com>
2019-10-02 11:36:30 -0700

    linkedlist

--- LeetcodeLinkedList/src/RemoveDuplicatesfromSortedList.java
+++ LeetcodeLinkedList/src/RemoveDuplicatesfromSortedList.java
@@ -1,5 +1,7 @@
 /**
  * Created by 79300 on 2019/10/2.
+ * 要注意的是如果current.val==current.next.val,删掉current.next的时候不需要移动current指针
+ * 移动的话变成current.next就是新的值了，这个值会被错过和上一个值比较
  */
 public class RemoveDuplicatesfromSortedList {
     public ListNode deleteDuplicates(ListNode head) {
