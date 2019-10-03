422dc6c121f8e48f0eed8a4af0ddfdef86fa8cca
Clover <xiaohangong04@gmail.com>
2019-10-03 15:20:03 -0700

    linkedlist

---
+++ LeetcodeLinkedList/src/MergeTwoSortedLists.java
@@ -0,0 +1,17 @@
+/**
+ * Created by 79300 on 2019/10/3.
+ * recursive
+ */
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if(l1.val>l2.val){
+            l2.next = mergeTwoLists(l2.next,l1);
+            return l2;
+        }else {
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }
+    }
+}
